45a5eeab2f4ce2eea3d3a8c673ab04cab299051e
kimmking <kimmking@apache.org>
2020-10-28 16:09:59 +0800

    initialize

---
+++ 01jvm/GCLogAnalysis.java
@@ -0,0 +1,63 @@
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
+/*
+演示GC日志生成与解读
+*/
+public class GCLogAnalysis {
+    private static Random random = new Random();
+    public static void main(String[] args) {
+        // 当前毫秒时间戳
+        long startMillis = System.currentTimeMillis();
+        // 持续运行毫秒数; 可根据需要进行修改
+        long timeoutMillis = TimeUnit.SECONDS.toMillis(1);
+        // 结束时间戳
+        long endMillis = startMillis + timeoutMillis;
+        LongAdder counter = new LongAdder();
+        System.out.println("正在执行...");
+        // 缓存一部分对象; 进入老年代
+        int cacheSize = 2000;
+        Object[] cachedGarbage = new Object[cacheSize];
+        // 在此时间范围内,持续循环
+        while (System.currentTimeMillis() < endMillis) {
+            // 生成垃圾对象
+            Object garbage = generateGarbage(100*1024);
+            counter.increment();
+            int randomIndex = random.nextInt(2 * cacheSize);
+            if (randomIndex < cacheSize) {
+                cachedGarbage[randomIndex] = garbage;
+            }
+        }
+        System.out.println("执行结束!共生成对象次数:" + counter.longValue());
+    }
+
+    // 生成对象
+    private static Object generateGarbage(int max) {
+        int randomSize = random.nextInt(max);
+        int type = randomSize % 4;
+        Object result = null;
+        switch (type) {
+            case 0:
+                result = new int[randomSize];
+                break;
+            case 1:
+                result = new byte[randomSize];
+                break;
+            case 2:
+                result = new double[randomSize];
+                break;
+            default:
+                StringBuilder builder = new StringBuilder();
+                String randomString = "randomString-Anything";
+                while (builder.length() < randomSize) {
+                    builder.append(randomString);
+                    builder.append(max);
+                    builder.append(randomSize);
+                }
+                result = builder.toString();
+                break;
+        }
+        return result;
+    }
+}
---
+++ 02nio/nio01/src/main/java/java0/nio01/HttpServer02.java
@@ -0,0 +1,39 @@
+package java0.nio01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer02 {
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSocket = new ServerSocket(8802);
+        while (true) {
+            try {
+                final Socket socket = serverSocket.accept();
+                new Thread(() -> {
+                    service(socket);
+                }).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    private static void service(Socket socket) {
+        try {
+            Thread.sleep(20);
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            String body = "hello,nio";
+            printWriter.println("Content-Length:" + body.getBytes().length);
+            printWriter.println();
+            printWriter.write(body);
+            printWriter.close();
+            socket.close();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
