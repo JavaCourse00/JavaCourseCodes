3a852c80c5025aa4b1c761def352dd3385aeeb00
kimmking <kimmking@163.com>
2020-12-12 17:02:49 +0800

    add rpc

---
+++ 07rpc/rpc01/rpcfx-api/src/main/java/io/kimmking/rpcfx/api/UserService.java
@@ -0,0 +1,7 @@
+package io.kimmking.rpcfx.api;
+
+public interface UserService {
+
+    User findById(int id);
+
+}
---
+++ 07rpc/rpc01/rpcfx-client/src/test/java/io/kimmking/springboot01/Springboot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kimmking.springboot01;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
