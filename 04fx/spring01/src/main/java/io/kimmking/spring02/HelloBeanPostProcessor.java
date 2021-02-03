fa2aa23cbf7beab359c0411582a05796b058c86e
kimmking <kimmking@163.com>
2021-02-03 23:35:18 +0800

    add BeanPostProcessor

---
+++ 04fx/spring01/src/main/java/io/kimmking/spring02/HelloBeanPostProcessor.java
@@ -0,0 +1,27 @@
+package io.kimmking.spring02;
+
+import io.kimmking.spring01.Student;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println(" ====> postProcessBeforeInitialization " + beanName  +":"+ bean);
+        // 可以加点额外处理
+        // 例如
+        if (bean instanceof Student) {
+            Student student = (Student) bean;
+            student.setName(student.getName() + System.currentTimeMillis());
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println(" ====> postProcessAfterInitialization " + beanName +":"+ bean);
+        return bean;
+    }
+}
